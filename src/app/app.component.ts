241c7ce69e60c1101c18598be5b6d70529a02507
Jason <extremewinds@gmail.com>
2025-07-18 05:46:35 +0200

    code reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {EmployeeModel} from './model/Employee';
 
