2db457f6c1cded4caf490720d84b6042a360a62f
Jason <extremewinds@gmail.com>
2025-07-19 10:45:26 +0200

    add edit and delete actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {EmployeeModel} from './model/Employee';
 
 @Component({
@@ -18,7 +18,6 @@ export class AppComponent {
 
   constructor() {
     this.createForm();
-    debugger;
     const oldData = localStorage.getItem('EmpData');
     if (oldData != null) {
       const parseData = JSON.parse(oldData);
@@ -29,26 +28,56 @@ export class AppComponent {
   createForm() {
     this.employeeForm = new FormGroup({
       employeeId: new FormControl(this.employeeObj.employeeId),
-      name: new FormControl(this.employeeObj.name),
+      name: new FormControl(this.employeeObj.name, [Validators.required]),
       city: new FormControl(this.employeeObj.city),
       state: new FormControl(this.employeeObj.state),
       emailId: new FormControl(this.employeeObj.emailId),
       contactNo: new FormControl(this.employeeObj.contactNo),
       address: new FormControl(this.employeeObj.address),
-      pinCode: new FormControl(this.employeeObj.pinCode)
+      pinCode: new FormControl(this.employeeObj.pinCode, [Validators.required, Validators.minLength(6)])
     })
   }
 
   onSave() {
-    debugger;
     const oldData = localStorage.getItem('EmpData');
     if (oldData != null) {
       const parseData = JSON.parse(oldData);
-      this.employeeForm.controls['employeeId'].setValue(parseData.length);
+      this.employeeForm.controls['employeeId'].setValue(parseData.length + 1);
       this.employeeList.unshift(this.employeeForm.value);
     } else {
+      this.employeeForm.controls['employeeId'].setValue(1);
       this.employeeList.unshift(this.employeeForm.value);
     }
     localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+    this.onReset()
+  }
+
+  onEdit(item: EmployeeModel) {
+    this.employeeObj = item;
+    this.createForm();
+  }
+
+  onUpdate() {
+    const record = this.employeeList.find(m=>m.employeeId == this.employeeForm.controls['employeeId'].value);
+    if(record != undefined) {
+      record.address = this.employeeForm.controls['address'].value;
+      record.name = this.employeeForm.controls['name'].value;
+      record.contactNo = this.employeeForm.controls['contactNo'].value;
+    }
+    localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+    this.onReset();
+  }
+
+  onDelete(item: EmployeeModel) {
+    const isDelete = confirm("Are you sure you want to delete?");
+    if (isDelete) {
+      this.employeeList = this.employeeList.filter(emp=> emp.employeeId != item.employeeId);
+      localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+    }
+  }
+
+  onReset() {
+    this.employeeObj = new EmployeeModel();
+    this.createForm();
   }
 }
